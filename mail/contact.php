cdf9c8da8926afb21f376abcd605cd388759706e
Obed Amoako <obedamoako92@gmail.com>
2022-05-13 10:19:28 +0000

    added validation to the php

--- mail/contact.php
+++ mail/contact.php
@@ -1,22 +1,22 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();
-}
+// if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+//   http_response_code(500);
+//   exit();
+// }
 
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email = strip_tags(htmlspecialchars($_POST['email']));
-$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+// $name = strip_tags(htmlspecialchars($_POST['name']));
+// $email = strip_tags(htmlspecialchars($_POST['email']));
+// $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+// $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "obedamoako92@gmail.com";
-$subject = "$m_subject:  $name";
-$body = "Hello Obed,\nSomeone sent you message from your website.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";
-try {
-  mail($to, $subject, $body, $header);
-} catch(_err) {
-  http_response_code(500);
-}
+// $to = "obedamoako92@gmail.com";
+// $subject = "$m_subject:  $name";
+// $body = "Hello Obed,\nSomeone sent you message from your website.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+// $header = "From: $email";
+// $header .= "Reply-To: $email";
+// try {
+//   mail($to, $subject, $body, $header);
+// } catch(_err) {
+//   http_response_code(500);
+// }
 ?>

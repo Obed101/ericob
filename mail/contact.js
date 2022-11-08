79987a2326121971ed55c9df0572cfb81a5d8b74
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2022-11-08 12:21:51 +0000

    chnaged message

--- mail/contact.js
+++ mail/contact.js
@@ -45,13 +45,13 @@ $(function() {
           $this.text($text);
         },
         error: function(e) {
-          $("#success").html("<div class='alert alert-danger'>");
-          $("#success > .alert-danger")
+          $("#success").html("<div class='alert alert-success'>");
+          $("#success > .alert-success")
             .html(
               `<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;`
             )
             .append("</button>");
-          $("#success > .alert-danger").append(
+          $("#success > .alert-success").append(
             $("<strong>").html(
               "Sorry " +
                 name +
@@ -65,7 +65,7 @@ or call me on my number below`
             )
           );
           console.log("send not successful");
-          $("#success > .alert-danger").append("</div>");
+          $("#success > .alert-success").append("</div>");
           console.log(e);
           $("#contactForm").trigger("reset");
           $this.text($text);
